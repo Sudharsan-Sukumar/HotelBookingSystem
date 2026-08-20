61da471abd8c6b472a3bb6917320924d9f6bf3bf
Sudharsan <itzz.sudharsan@gmail.com>
2026-08-20 23:46:48 +0530

    Update apiUrl for production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://hotelbooking-capstone-api-d9beh4f7c0fuetf8.centralindia-01.azurewebsites.net/api',
   googleClientId: ''
 };
